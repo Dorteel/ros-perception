87d9df9d18978031a15372025a704053fb476531
Mark Adamik <m.adamik@vu.nl>
2022-05-09 14:13:13 +0200

    Before deconstruction: a verison where Yolo still works

--- scripts/detect.py
+++ scripts/detect.py
@@ -199,43 +199,43 @@ def run(
         strip_optimizer(weights)  # update model (to fix SourceChangeWarning)
 
 
-def parse_opt():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'yolov5s.pt', help='model path(s)')
-    parser.add_argument('--source', type=str, default=ROOT / 'yolov5/data/images', help='file/dir/URL/glob, 0 for webcam')
-    parser.add_argument('--data', type=str, default=ROOT / 'yolov5/data/coco128.yaml', help='(optional) dataset.yaml path')
-    parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[640], help='inference size h,w')
-    parser.add_argument('--conf-thres', type=float, default=0.25, help='confidence threshold')
-    parser.add_argument('--iou-thres', type=float, default=0.45, help='NMS IoU threshold')
-    parser.add_argument('--max-det', type=int, default=1000, help='maximum detections per image')
-    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--view-img', action='store_true', help='show results')
-    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
-    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
-    parser.add_argument('--save-crop', action='store_true', help='save cropped prediction boxes')
-    parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
-    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --classes 0, or --classes 0 2 3')
-    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
-    parser.add_argument('--augment', action='store_true', help='augmented inference')
-    parser.add_argument('--visualize', action='store_true', help='visualize features')
-    parser.add_argument('--update', action='store_true', help='update all models')
-    parser.add_argument('--project', default=ROOT / 'runs/detect', help='save results to project/name')
-    parser.add_argument('--name', default='exp', help='save results to project/name')
-    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
-    parser.add_argument('--line-thickness', default=3, type=int, help='bounding box thickness (pixels)')
-    parser.add_argument('--hide-labels', default=False, action='store_true', help='hide labels')
-    parser.add_argument('--hide-conf', default=False, action='store_true', help='hide confidences')
-    parser.add_argument('--half', action='store_true', help='use FP16 half-precision inference')
-    parser.add_argument('--dnn', action='store_true', help='use OpenCV DNN for ONNX inference')
-    opt = parser.parse_args()
-    opt.imgsz *= 2 if len(opt.imgsz) == 1 else 1  # expand
-    print_args(vars(opt))
-    return opt
+# def parse_opt():
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'yolov5s.pt', help='model path(s)')
+#     parser.add_argument('--source', type=str, default=ROOT / 'yolov5/data/images', help='file/dir/URL/glob, 0 for webcam')
+#     parser.add_argument('--data', type=str, default=ROOT / 'yolov5/data/coco128.yaml', help='(optional) dataset.yaml path')
+#     parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[640], help='inference size h,w')
+#     parser.add_argument('--conf-thres', type=float, default=0.25, help='confidence threshold')
+#     parser.add_argument('--iou-thres', type=float, default=0.45, help='NMS IoU threshold')
+#     parser.add_argument('--max-det', type=int, default=1000, help='maximum detections per image')
+#     parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+#     parser.add_argument('--view-img', action='store_true', help='show results')
+#     parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+#     parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
+#     parser.add_argument('--save-crop', action='store_true', help='save cropped prediction boxes')
+#     parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
+#     parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --classes 0, or --classes 0 2 3')
+#     parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+#     parser.add_argument('--augment', action='store_true', help='augmented inference')
+#     parser.add_argument('--visualize', action='store_true', help='visualize features')
+#     parser.add_argument('--update', action='store_true', help='update all models')
+#     parser.add_argument('--project', default=ROOT / 'runs/detect', help='save results to project/name')
+#     parser.add_argument('--name', default='exp', help='save results to project/name')
+#     parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+#     parser.add_argument('--line-thickness', default=3, type=int, help='bounding box thickness (pixels)')
+#     parser.add_argument('--hide-labels', default=False, action='store_true', help='hide labels')
+#     parser.add_argument('--hide-conf', default=False, action='store_true', help='hide confidences')
+#     parser.add_argument('--half', action='store_true', help='use FP16 half-precision inference')
+#     parser.add_argument('--dnn', action='store_true', help='use OpenCV DNN for ONNX inference')
+#     opt = parser.parse_args()
+#     opt.imgsz *= 2 if len(opt.imgsz) == 1 else 1  # expand
+#     print_args(vars(opt))
+#     return opt
 
 
-def main(opt):
-    check_requirements(exclude=('tensorboard', 'thop'))
-    run(**vars(opt))
+# def main(opt):
+#     check_requirements(exclude=('tensorboard', 'thop'))
+#     run(**vars(opt))
 
 
 
@@ -301,8 +301,6 @@ class cvBridgeDemo():
         self.topic = topic
         # Create the OpenCV display window for the RGB image
         self.cv_window_name = self.node_name
-        #cv2.NamedWindow(self.cv_window_name, cv2.CV_WINDOW_NORMAL)
-        #cv2.MoveWindow(self.cv_window_name, 25, 75)
 
         # Create the cv_bridge object
         self.bridge = CvBridge()
@@ -323,8 +321,7 @@ class cvBridgeDemo():
 
         # Convert the image to a Numpy array since most cv2 functions
         # require Numpy arrays.
-        run(source=frame)
-        print(len(frame))
+        #run(source=frame)
         frame = np.array(frame, dtype=np.uint8)
 
         # Display the image.
@@ -342,35 +339,16 @@ class cvBridgeDemo():
         print("Shutting down vision node.")
         cv2.destroyAllWindows()   
 
-## Uncommented for tutorial
-# if __name__ == "__main__":
-#     # opt = parse_opt()
-#     # main(opt)
-
-#     # Go to class functions that do all the heavy lifting. Do error checking.
-#     try:
-#         # topics = rospy.get_published_topics()
-#         # for topic, msg in topics:
-#         #     print(topic, msg)
-#         ne = RosNode('testing',
-#             sub_topic="/locobot/camera/color/image_raw",
-#             pub_topic="test_image",
-#             sub_msg_type=Image,
-#             pub_msg_type=Image)
-#         ne.start()
-#     except rospy.ROSInterruptException:
-#         pass
-
-
 def main(args):
-    depthTopic = "/locobot/camera/depth/image_rect_raw"
-    colorTopic = "/locobot/camera/color/image_raw"       
-    try:
-        cvBridgeDemo(topic=colorTopic)
-        rospy.spin()
-    except KeyboardInterrupt:
-        print("Shutting down vision node.")
-        cv2.DestroyAllWindows()
+    run(source=0)
+    # depthTopic = "/locobot/camera/depth/image_rect_raw"
+    # colorTopic = "/locobot/camera/color/image_raw"       
+    # try:
+    #     cvBridgeDemo(topic=colorTopic)
+    #     rospy.spin()
+    # except KeyboardInterrupt:
+    #     print("Shutting down vision node.")
+    #     cv2.DestroyAllWindows()
 
 if __name__ == '__main__':
     main(sys.argv)
